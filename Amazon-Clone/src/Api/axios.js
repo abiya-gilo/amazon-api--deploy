382c53a6fd52c726f7080568738243be3d1872b4
abiya-gilo <bekabiya@gmail.com>
2025-12-28 12:03:31 -0500

    Deploying on netlify

--- Amazon-Clone/src/Api/axios.js
+++ Amazon-Clone/src/Api/axios.js
@@ -1,10 +1,14 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
+  // local instance of firebase function
   // baseURL:
-    // import.meta.env.VITE_BACKEND_URL ||
-    // "http://127.0.0.1:5001/clone-6181a/us-central1/api",
-  baseURL: "https://us-central1-clone-6181a.cloudfunctions.net/api"
+  // import.meta.env.VITE_BACKEND_URL ||
+  // "http://127.0.0.1:5001/clone-6181a/us-central1/api",
+  // deployed version of firebase function
+  baseURL: "https://us-central1-clone-6181a.cloudfunctions.net/api",
+  // deployed version of amazon server on render.com
+  // baseURL: "https://amazon-api-deploy-6bdm.onrender.com",
 });
 
 export { axiosInstance };
